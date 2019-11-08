2d2622eb096055e7fa73228dc30e040ef52810a7
andreaskullberg <andreas.t:kullberg@hotmail.com>
2019-11-08 15:06:45 +0100

    Finished project

---
+++ Labb2/Position.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace Labb2
+{
+    public class Position
+    {
+        private int _x;
+        private int _y;
+
+        public int X {
+            get => _x;
+            set => _x = Math.Abs(value);          
+            
+        }
+
+        public int Y
+        {
+            get => _y;
+            set => _y = Math.Abs(value);
+
+        }
+
+        public Position(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public double Lenght() => Math.Sqrt(Square(X) + Square(Y));
+        
+        public static double Square(int number) => Math.Pow(number, 2);
+
+        public bool Equals (Position p) => p.Y == this.Y && p.X == this.X;
+       
+        public Position Clone() => new Position(this.X, this.Y);
+       
+        public override string ToString()
+        {
+            return $"({this.X},{this.Y})";
+        }
+
+        public static bool operator >(Position p1, Position p2)
+        {
+            if (p1.Lenght() == p2.Lenght())
+            {
+                return p1.X > p2.X;
+            }
+            return p1.Lenght() > p2.Lenght();
+        }
+
+        public static bool operator <(Position p1, Position p2)
+        {
+            if (p1.Lenght() == p2.Lenght())
+            {
+                return p1.X < p2.X;
+            }
+            return p1.Lenght() < p2.Lenght();
+        }
+
+        public static Position operator +(Position p1, Position p2) => new Position(p1.X + p2.X, p1.Y + p2.Y);
+
+        public static Position operator -(Position p1, Position p2) => new Position(p1.X - p2.X, p1.Y - p2.Y);
+
+        public static double operator %(Position p1, Position p2) => Math.Sqrt(Square(p1.X - p2.X) + Square(p1.Y - p2.Y));
+       
+    }
+}
---
+++ Labb2/SortedPosList.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Labb2
+{
+    public class SortedPosList
+    {
+        public List<Position> Positions = new List<Position>();
+
+        public Position this[int key]
+        {
+            get => this.Positions[key];
+        }
+
+        public SortedPosList()
+        {
+        }
+
+        public int count() => Positions.Count;
+
+        public void Add(Position p)
+        {
+            Positions.Insert(0, p);
+
+            for (int i = 0; i < Positions.Count-1; i++)
+            {
+
+                if (Positions[i] > Positions[i+1])
+                {
+                    var temp = Positions[i + 1];
+                    Positions[i + 1] = Positions[i];
+                    Positions[i] = temp;
+                }
+            }           
+        }
+
+        public bool Remove(Position p)
+        {
+            for (int i = 0; i < Positions.Count; i++)
+            {
+                if (Positions[i].Equals(p))
+                {
+                    Positions.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public SortedPosList Clone()
+        {
+            SortedPosList sortedPosList = new SortedPosList();
+            for (int i = 0; i < Positions.Count; i++)
+            {
+                sortedPosList.Add(Positions[i].Clone());
+            }
+            return sortedPosList;
+        }
+
+        public SortedPosList CircleContent(Position centerPos, double radius)
+        {
+            SortedPosList sortedPosList = new SortedPosList();
+            for (int i = 0; i < Positions.Count; i++)
+            {
+                if (Math.Pow(Positions[i].X - centerPos.X, 2) + Math.Pow(Positions[i].Y - centerPos.Y, 2) < Math.Pow(radius, 2))
+                {
+                    sortedPosList.Add(Positions[i].Clone());
+                }
+            }
+            return sortedPosList;
+        }
+
+        public static SortedPosList operator +(SortedPosList sp1, SortedPosList sp2)
+        {
+            SortedPosList sortedPosList = sp1.Clone();
+            for (int i = 0; i < sp2.Positions.Count; i++)
+            {
+                sortedPosList.Add(sp2.Positions[i].Clone());
+            }
+            return sortedPosList;
+
+        }
+
+        public static SortedPosList operator -(SortedPosList sp1, SortedPosList sp2)
+        {
+            SortedPosList sortedPosList = sp1.Clone();
+            int i = 0;
+            int j = 0;
+
+            while(i < sp1.Positions.Count && j < sp2.Positions.Count)
+            {
+                if (sp1[i].Equals(sp2[j]))
+                {
+                    sortedPosList.Remove(sp1[i]);
+                    i++;
+                    j++;
+                    
+                }
+                else if (sp1[i] < sp2[j])
+                {
+                    i++;
+                }
+                else
+                {
+                    j++;
+                }
+
+            }
+            return sortedPosList;
+
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", Positions);
+        }
+    }
+}
